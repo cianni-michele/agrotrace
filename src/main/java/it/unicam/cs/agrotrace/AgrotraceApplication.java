21d59299b4f7d5a7b7bc8167c541010e9178b9dd
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-05-13 10:49:46 +0200

    Init project

---
+++ src/main/java/it/unicam/cs/agrotrace/AgrotraceApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.agrotrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrotraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrotraceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unicam/cs/agrotrace/AgrotraceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.agrotrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrotraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
