d00106f4603084b612e163cbe0db0489967aa15e
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-06-04 12:14:42 +0200

    Add development-specific security configurations for H2 console and API documentation access

--- src/main/java/it/unicam/cs/agrotrace/config/security/SecurityConfig.java
+++ src/main/java/it/unicam/cs/agrotrace/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package it.unicam.cs.agrotrace.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -20,12 +21,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Profile("dev")
+    public SecurityFilterChain devFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(authorizeRequests -> {
-                    authorizeRequests.requestMatchers("/h2-console/**").permitAll();
+                    authorizeRequests.requestMatchers(
+                            "/h2-console/**",
+                            "/swagger-ui.html",
+                            "/swagger-ui/**",
+                            "/v3/api-docs/**",
+                            "/api/v1/**"
+                    ).permitAll();
                     authorizeRequests.requestMatchers("/api/content/").authenticated();
                     authorizeRequests.requestMatchers("/api/content/{contentId}/validate").hasRole("CURATOR");
                     authorizeRequests.anyRequest().authenticated();
@@ -35,7 +43,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authManager(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
+    @Profile("dev")
+    @SuppressWarnings("deprecation") // NoOpPasswordEncoder is deprecated but used here for simplicity in development
+    public AuthenticationManager devAuthManager(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
         AuthenticationManagerBuilder autheManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
 
         autheManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
