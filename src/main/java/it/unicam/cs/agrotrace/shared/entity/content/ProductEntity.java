8c966afbf8979ab7a15b73c9246cbb6821f4a9bb
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-06-13 02:39:41 +0200

    Add price, quantity, images, and certifications fields to ProductEntity

--- src/main/java/it/unicam/cs/agrotrace/shared/entity/content/ProductEntity.java
+++ src/main/java/it/unicam/cs/agrotrace/shared/entity/content/ProductEntity.java
@@ -1,7 +1,30 @@
 package it.unicam.cs.agrotrace.shared.entity.content;
 
+import it.unicam.cs.agrotrace.shared.entity.file.CertificationEntity;
+import it.unicam.cs.agrotrace.shared.entity.file.ImageEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
 @Entity(name = "products")
 public class ProductEntity extends ContentEntity {
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @JoinColumn(name = "images", nullable = false)
+    private List<ImageEntity> images;
+
+    @JoinColumn(name = "certifications", nullable = false)
+    private List<CertificationEntity> certifications;
+
 }
