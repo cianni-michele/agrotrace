a8ad642cce854d55ea0bc8d582d4e8f9864a52bb
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-06-15 12:04:53 +0200

    Add support for Producer user in CustomUserDetailsService

--- src/main/java/it/unicam/cs/agrotrace/service/CustomUserDetailsService.java
+++ src/main/java/it/unicam/cs/agrotrace/service/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package it.unicam.cs.agrotrace.service;
 
 import it.unicam.cs.agrotrace.shared.model.user.admin.Curator;
+import it.unicam.cs.agrotrace.shared.model.user.author.Producer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,9 +10,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    /* This is a simple example. In a real application, you would fetch user details from a database. */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // This is a simple example. In a real application, you would fetch user details from a database.
         if (username.equals("curator")) {
             return Curator.builder()
                     .id(0L)
@@ -20,6 +21,16 @@ public class CustomUserDetailsService implements UserDetailsService {
                     .password("{noop}password") // {noop} indicates no password encoding
                     .build();
         }
+
+        if (username.equals("producer")) {
+            return Producer.builder()
+                    .id(2L)
+                    .name("producer")
+                    .username("producer")
+                    .password("{noop}password") // {noop} indicates no password encoding
+                    .build();
+        }
+
         throw new UsernameNotFoundException("User not found with username: " + username);
     }
 }
